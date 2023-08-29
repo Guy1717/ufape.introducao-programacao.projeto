c2dfdf9d7a0a3cba16deef726af23b5a86073337
Guy176 <86077955+Guy1717@users.noreply.github.com>
2023-08-29 00:00:34 -0300

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,83 +1,193 @@
-class game:
-    def __init__(self, name, price, yearLaunch):
-        self.name = name
-        self.price = price
-        self.yearLaunch = yearLaunch
+from exceptions import InvalidScore
 
+def getList(filename):
+    file = open(filename, 'r')
+    games = []
 
-def readParameters(option):
-    parameters = {}
+    for line in file:
+        params = line.split(';')
+        name, year, score = params[0], params[1], params[2]
+        games.append({'name': name, 'year': year, 'score': score})
+    
+    file.close()
 
-    if option == 1:
-        name = input("Insira o nome do jogo: ")
-        parameters['name'] = name
-    elif option == 2:
-        price = input("Insira o preço do jogo: ")
-        parameters['price'] = float(price)
-    elif option == 3:
-        year = input("Insira o ano de lançamento do jogo: ")
-        parameters['year'] = int(year)
-    elif option == 4:
-        name = input("Insira o nome do jogo: ")
-        price = input("Insira o preço do jogo: ")
-        year = input("Insira o ano de lançamento do jogo: ")
-        parameters['name'] = name
-        parameters['price'] = float(price)
-        parameters['year'] = int(year)
+    return games
 
-    return parameters
+def readName():
+    name = input('Nome: ')
+    return name
 
+def readYear():
+    year = int(input('Ano de lançamento: '))
+    return year
 
-def register(gameList):
-    param = readParameters(4)
-    name, price, year = param['name'], param['price'], param['year']
+def readScore():
+    score = int(input('Pontuação (1 ~ 10): '))
 
-    gameList.append(game(name, price, year))
+    if score < 0 or score > 10:
+        raise InvalidScore('Pontuação deve ser entre 0 e 10')
+    return score
 
+def readParams():
+    name = readName()
+    year = readYear()
+    score = readScore()
+    
+    params = [name,year,score]
 
-def show(gameList):
-    print('================================================================')
-    for e in gameList:
-        print(f"{gameList.index(e)+1}: {e.name} ; R${e.price} : {e.yearLaunch}")
-    print('================================================================')
+    return params
 
+def register(filename):
+    try:
+        games = getList(filename)
 
-def exists(gameList):
-    bool = False
-    search = input("Buscar por nome: ")
-    for e in gameList:
-        if e.name == search:
-            bool = True
+        newData = readParams()
+        name, year, score = newData[0], newData[1], newData[2]
 
-    if bool:
-        print(f"{search} está na lista!")
-    else:
-        print(f"{search} não está na lista!")
+        file = open(filename, 'w')
+        for game in games:
+            file.write(f"{game['name']};{game['year']};{game['score']}")
+            
+        file.write(f"{name};{year};{score}\n")
+        file.close()
 
+    except FileNotFoundError:
+        file = open(filename, 'w')
+        
+        newData = readParams()
+        name, year, score = newData[0], newData[1], newData[2]
+        
+        file.write(f"{name};{year};{score}\n")
+        file.close()
+    except InvalidScore:
+        print('[!] Pontuação deve ser entre 0 e 10')
+    except:
+        print("[!] Erro inesperado ao registrar item")
 
-def delete(gameList):
+def show(filename):
+    try:
+        games = getList(filename)
 
-    show(gameList)
+        i=1
+        print('================================================================')
+        for game in games:
+            print(f"[{i}]: {game['name']} | {game['year']} | {game['score']}")
+            i += 1
+        print('================================================================')
+    except FileNotFoundError:
+        print('[!] Arquivo inexistente')
+    except:    
+        print('[!] Erro inesperado ao mostrar lista')
 
-    index = int(input("Qual item deseja remover?\n"))-1
-    removedItem = gameList.pop(index).name
+def edit(filename):
+    try:
+        games = getList(filename)
+        show(filename)
 
-    print(f"Item {index+1} ({removedItem}) removido!")
+        index = int(input('Qual item deseja editar? '))-1
 
+        print("1. Nome\n2. Ano\n3. Pontuação\n4. Todos\n")
+        change = int(input('O que deseja editar? '))
 
-def update(gameList):
+        if change == 1:
+            games[index]['name'] = readName()
+        elif change == 2:
+            games[index]['year'] = readYear()
+        elif change == 3:
+            games[index]['score'] = f"{readScore()}\n"
+        elif change == 4:
+            params = readParams()
+            name, year, score = params[0], params[1], params[2]
 
-    show(gameList)
-    index = int(input("Qual item deseja atualizar?\n"))-1
+            games[index]['name'], games[index]['year'], games[index]['score'] = name, year, score
 
-    editParam = int(input(
-        "O que deseja atualizar?\n1. Nome\n2. Preço\n3. Ano de lançamento\n4. Todas as opçoes\n"))
+        file = open(filename, 'w')
+        for game in games:
+            file.write(f"{game['name']};{game['year']};{game['score']}")
 
-    param = readParameters(editParam)
+        file.close()
+    except InvalidScore:
+        print('[!] Pontuação deve ser entre 0 e 10')
+    except:
+        print("[!] Erro inesperado ao editar item")
 
-    if 'name' in param:
-        gameList[index].name = param['name']
-    if 'price' in param:
-        gameList[index].price = param['price']
-    if 'year' in param:
-        gameList[index].yearLaunch = param['year']
+def delete(filename):
+    try:
+        show(filename)
+        
+        games = getList(filename)
+
+        index = int(input('Qual item deseja deletar? '))-1
+
+        deleted = games.pop(index)
+        print(f"Item {index} ({deleted['name']}) deletado com sucesso!")
+
+        file = open(filename, 'w')
+        for game in games:
+            file.write(f"{game['name']};{game['year']};{game['score']}")
+        file.close()
+
+    except:
+        print('[!] Erro inesperado ao deletar item')
+
+def search(filename):
+    games = getList(filename)
+
+    # Escolher propriedade de busca
+    print('1. Nome\n2. Ano\n3. Pontuação')
+    searchBy = int(input('Selecione uma opção: '))
+
+    # Name
+    if searchBy == 1:
+        search = input('Nome do jogo para buscar: ')
+        itemExists = False
+        for game in games:
+            if game['name'] == search:
+                index = games.index(game)
+                itemExists = True
+
+        if itemExists:
+            print(f'» Item presente na lista')
+            game = games[index]
+            name,year,score = game['name'], game['year'], game['score']
+            print(f"{name} | {year} | {score}")
+        else:
+            print('» Item inexistente')
+    # Year
+    elif searchBy == 2:
+        search = int(input('Ano do jogo para buscar: '))
+        found = []
+
+        for game in games:
+            if int(game['year']) == search:
+                index = games.index(game)
+                found.append(f"[{index+1}]: {game['name']} | {game['year']} | {game['score']}")
+        if len(found) > 0:
+            print("» Itens encontrados: ")
+            for game in found:
+                print(game)
+        else:
+            print("» Item inexistente")
+    # Score
+    elif searchBy == 3:
+        search = int(input('Pontuação do jogo para buscar: '))
+        
+def run(filename):
+    while(True):
+        print("1. Mostrar lista\n2. Registrar item\n3. Editar item\n4. Deletar item\n5. Buscar item (nome)\n0. Encerrar programa")
+        option = int(input('Selecione uma opção: '))
+
+        if option == 0:
+            exit()
+        elif option == 1:
+            show(filename)
+        elif option == 2:
+            register(filename)
+        elif option == 3:
+            edit(filename)
+        elif option == 4:
+            delete(filename)
+        elif option == 5:
+            search(filename)
+        else:
+            print('Opção inválida')
--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-from functions import register, show, delete, exists, update, game
+from functions import run
 
-games = []
+filename = "file.csv"
 
-example = game("Game", 100.00, 1999)
-games.append(example)
-
-while (True):
-    print("1. Apresentar jogos")
-    print("2. Registrar jogo")
-    print("3. Atualizar jogo")
-    print("4. Buscar jogo (nome)")
-    print("5. Deletar jogo")
-
-    x = int(input("Escolha uma opção: "))
-
-    if x == 1:
-        show(games)
-    if x == 2:
-        register(games)
-    if x == 3:
-        update(games)
-    if x == 4:
-        exists(games)
-    if x == 5:
-        delete(games)
+run(filename)
